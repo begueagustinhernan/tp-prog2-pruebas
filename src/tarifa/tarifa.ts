a9b59c124a83841370de88fa0b94255b362ed90b
Pablo <pablo.duval91@gmail.com>
2025-11-10 23:54:57 -0300

    se agregan modificaciones sobre clase reserva y tarifas para incorporar estrategia de temporadas

--- src/tarifa/tarifa.ts
+++ src/tarifa/tarifa.ts
@@ -1,20 +1,31 @@
 import Kilometraje from "../kilometraje";
+import { IEstrategiaTarifaTemporada } from "../temporadas/iEstrategiaTarifaTemporada";
 
 export abstract class Tarifa {
     protected tarifaBase: number;
     protected cargoPorKmRecorrido: number;
+    private estrategiaTemporada: IEstrategiaTarifaTemporada;
 
-    constructor() {
+    constructor(estrategiaTemporada: IEstrategiaTarifaTemporada) {
         this.tarifaBase = 0;
         this.cargoPorKmRecorrido = 0;
+        this.estrategiaTemporada = estrategiaTemporada;
     }
 
     public getTarifaBase(): number {
         return this.tarifaBase;
     }
 
+    public getTarifaBaseAjustada(): number {
+        return this.estrategiaTemporada.ajustarTarifaBase(this.tarifaBase);
+    }
+
     public getCargoPorKmRecorrido(): number {
         return this.cargoPorKmRecorrido;
+    }
+
+    public setEstrategiaTemporada(estrategia: IEstrategiaTarifaTemporada): void {
+        this.estrategiaTemporada = estrategia;
     }
 
     public abstract calcularCosto(duracionReserva: number, kilometrosRecorridos: Kilometraje): number;
--- src/tarifa/tarifaSedan.ts
+++ src/tarifa/tarifaSedan.ts
@@ -1,20 +1,21 @@
 import { Tarifa } from "./tarifa";
 import Kilometraje from "../kilometraje";
+import { IEstrategiaTarifaTemporada } from "../temporadas/iEstrategiaTarifaTemporada";
 
 const TARIFA_BASE_DIA = 50;
 const CARGO_ADICIONAL = 0.20;
 
 export default class TarifaSedan extends Tarifa {
 
-    constructor() {
-        super();
+    constructor(estrategiaTemporada: IEstrategiaTarifaTemporada) {
+        super(estrategiaTemporada);
         this.tarifaBase = TARIFA_BASE_DIA;
         this.cargoPorKmRecorrido = CARGO_ADICIONAL;
     }
 
     public calcularCosto(duracionReserva: number, kilometrosRecorridos: Kilometraje): number {
 
-        let costoBaseTotal: number = duracionReserva * this.getTarifaBase();
+        let costoBaseTotal: number = duracionReserva * this.getTarifaBaseAjustada();
         let costoVariableTotal: number = this.getCargoPorKmRecorrido() * kilometrosRecorridos.calcularKmsTotalesRecorridos();
 
         const costoTotal = costoBaseTotal + costoVariableTotal;
